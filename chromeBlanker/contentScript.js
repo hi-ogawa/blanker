37fc58ba8fb0a358360aa9c01c2a33faf8621115
hiogawa <hi.ogawa.zz@gmail.com>
2015-06-24 19:54:33 +0900

    takes care of br tag, make it run with stanford pos tagger server

--- chromeBlanker/contentScript.js
+++ chromeBlanker/contentScript.js
@@ -1,3 +1,60 @@
+var i = 0;
+function makeBlankableDom($dom, xml){
+    i++;
+    var col_before = $dom.css("color");
+    var col_after = $dom.css("background-color");
+
+    var class_name = "blank-paragraph" + i;
+    var $par = $("<span>").attr("id", class_name);
+    $(xml).find("s").each(function(){
+	$(this).find("wt").each(function(){
+	    var pos = $(this).find("pos").text();
+	    var word = $(this).find("word").text();
+	    if( pos.match(/^JJ/) ){
+		$par.append($("<span>").attr("class", "blank")
+			    .text(word));
+	    }else if(pos.match(/^VB/)){
+		$par.append($("<span>").attr("class", "blank")
+			    .text(word));
+	    }else if(pos.match(/^\-LRB\-$/)){
+		$par.append("(")
+	    }else if(pos.match(/^\-RRB\-$/)){
+		$par.append(")")
+	    }else{
+		$par.append(word);
+	    }
+	    $par.append(" ");
+	});
+    });
+
+    $par.find("span.blank").css({"border-bottom": "1px solid",
+				 "border-bottom-color": col_before,
+				 "color": col_after});
+
+    $par.find("span.blank").click(function(){
+	if( $(this).css("color") == col_before ){
+	    $(this).css("color", col_after);
+	}else{
+	    $(this).css("color", col_before);
+	}
+    });
+
+    var b = true;
+    $par.click(function(e){
+    	e.stopPropagation();
+    	if(e.altKey){
+    	    if(b){
+    	    	$par.find("span.blank").css("color", col_before);
+    	    }else{
+    	    	$par.find("span.blank").css("color", col_after);
+    	    }
+    	    b = !b;
+    	}
+    });
+
+    $dom.append($par);
+}
+
 function listParentDoms($elem){
     console.log("---- list parent Doms ----");
     $elem.parents().slice(0,5).each(function(){
@@ -20,64 +77,24 @@ $(function(){
 
 	    listParentDoms($(this));
 
+	    // should any style of <br> (like <br />, <br/>) be covered ??
+	    var txts = ($(this).html()).split("<br>")
+                        .filter(function(x){ return x.trim() != ""; })
+                        .map(function(x){ return $("<div>" + x + "</div>").text(); });
 
-	    chrome.runtime.sendMessage({
-    		url: "http://localhost:3000/cont0/blank",
-    		type: "0",
-    		sentence: $(this).text()
-	    }, function(responseText) {
-    		console.log("--- post response ---");
-    		// console.log(responseText);
-		var xml = $.parseXML(responseText);
+	    var $dom = $($(tag)[index]);
+	    $dom.empty();
+	    txts.forEach(function(txt){
+		chrome.runtime.sendMessage({
+    		    url: "http://localhost:3000/cont0/blank",
+    		    type: "0",
+    		    sentence: txt
+		}, function(responseText) {
+    		    console.log("--- post response ---");
+		    var xml = $.parseXML(responseText);
 
-		var $dom = $($(tag)[index]);
-		$dom.empty();
-		$(xml).find("s").each(function(){
-		    $(this).find("wt").each(function(){
-			var pos = $(this).find("pos").text();
-			var word = $(this).find("word").text();
-			if( pos.match(/^JJ/) ){
-			    $dom.append($("<span>").attr("class", "blank")
-			     	                   .text(word));
-			}else if(pos.match(/^VB/)){
-			    $dom.append($("<span>").attr("class", "blank")
-			     	                   .text(word));
-			}else if(pos.match(/^\-LRB\-$/)){
-			    $dom.append("(")
-			}else if(pos.match(/^\-RRB\-$/)){
-			    $dom.append(")")
-			}else{
-			    $dom.append(word);
-			}
-			$dom.append(" ");
-		    });
-		});
-
-		var col_before = $dom.find("span.blank").css("color");
-		var col_after = $dom.css("background-color");
-		$dom.find("span.blank").css({"border-bottom": "1px solid",
-					     "border-bottom-color": col_before,
-					     "color": col_after});
-		
-		$dom.find("span.blank").click(function(){
-		    if( $(this).css("color") == col_before ){
-			$(this).css("color", col_after);		
-		    }else{
-			$(this).css("color", col_before);
-		    }
-		});
-
-		var b = true;
-		$dom.click(function(e){
-		    e.stopPropagation();
-		    if(e.altKey){
-			if(b){
-			    $dom.find("span.blank").css("color", col_before);
-			}else{
-			    $dom.find("span.blank").css("color", col_after);
-			}
-			b = !b;
-		    }
+		    makeBlankableDom($dom, xml);
+		    $dom.append("<br>");
 		});
 	    });
 	}
--- railsBlanker/app/controllers/cont0_controller.rb
+++ railsBlanker/app/controllers/cont0_controller.rb
@@ -1,29 +1,50 @@
 require 'nokogiri'
+require 'socket'
 
 class Cont0Controller < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
-  def blank
-    # logger.debug "------ debug: cont0#blank -----"
-    # logger.debug params[:sentence]
+  # def blank
+  #   # logger.debug "------ debug: cont0#blank -----"
+  #   # logger.debug params[:sentence]
     
-    input = Tempfile.new('input')
-    input << params[:sentence]
-    input.close
+  #   input = Tempfile.new('input')
+  #   input << params[:sentence]
+  #   input.close
 
-    tagger_path = Pathname.new(Rails.root).parent + "stanford-postagger-2011-04-20"
-    # logger.debug "------ running tagger -----"
-    output = `java -cp #{tagger_path + "stanford-postagger.jar"} edu.stanford.nlp.tagger.maxent.MaxentTagger -model #{tagger_path + "models/left3words-wsj-0-18.tagger"} -textFile #{input.path}`
-    input.unlink
+  #   tagger_path = Pathname.new(Rails.root).parent + "stanford-postagger-2011-04-20"
+  #   # logger.debug "------ running tagger -----"
+  #   output = `java -cp #{tagger_path + "stanford-postagger.jar"} edu.stanford.nlp.tagger.maxent.MaxentTagger -model #{tagger_path + "models/left3words-wsj-0-18.tagger"} -textFile #{input.path}`
+  #   input.unlink
 
-    logger.debug "------ hopefully tagged sentence -----"
+  #   logger.debug "------ hopefully tagged sentence -----"
+  #   logger.debug output
+
+  #   render :text => split_tag(output)
+  # end
+
+  def blank
+    logger.debug params[:sentence]
+    input = params[:sentence].tr("\n"," ")
+    
+    output = call_tagger(input)
+
     logger.debug output
-
     render :text => split_tag(output)
   end
 
-  
   private
+
+  def call_tagger(input_str)
+    # you have to run the pos tagger server like this
+    # prompt> cd blanker/stanford-postagger-2011-04-20
+    # prompt> java -mx300m -cp stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTaggerServer -model models/left3words-wsj-0-18.tagger -port 3010
+    s = TCPSocket.open('localhost', 3010)
+    s.puts(input_str)
+    output_str = s.gets
+    s.close
+    return output_str
+  end
 
   def split_tag(output)
 
