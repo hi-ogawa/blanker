c96494eb40c2d8f9737c6ebf636c4bd2c40e8cb5
hiogawa <hi.ogawa.zz@gmail.com>
2015-06-24 16:58:57 +0900

    incorporates stanford pos tagger

--- chromeBlanker/contentScript.js
+++ chromeBlanker/contentScript.js
@@ -1,14 +1,3 @@
-function postRequest(s){
-    chrome.runtime.sendMessage({
-	url: "http://localhost:3000/cont0/blank",
-	type: "0",
-	sentence: s
-    }, function(responseText) {
-	console.log("--- post response ---");
-	console.log(responseText);
-    });
-}
-
 function listParentDoms($elem){
     console.log("---- list parent Doms ----");
     $elem.parents().slice(0,5).each(function(){
@@ -31,20 +20,79 @@ $(function(){
 
 	    listParentDoms($(this));
 
-	    postRequest($(this).text());
+
+	    chrome.runtime.sendMessage({
+    		url: "http://localhost:3000/cont0/blank",
+    		type: "0",
+    		sentence: $(this).text()
+	    }, function(responseText) {
+    		console.log("--- post response ---");
+    		// console.log(responseText);
+		var xml = $.parseXML(responseText);
+
+		var $dom = $($(tag)[index]);
+		$dom.empty();
+		$(xml).find("s").each(function(){
+		    $(this).find("wt").each(function(){
+			var pos = $(this).find("pos").text();
+			var word = $(this).find("word").text();
+			if( pos.match(/^JJ/) ){
+			    $dom.append($("<span>").attr("class", "blank")
+			     	                   .text(word));
+			}else if(pos.match(/^VB/)){
+			    $dom.append($("<span>").attr("class", "blank")
+			     	                   .text(word));
+			}else if(pos.match(/^\-LRB\-$/)){
+			    $dom.append("(")
+			}else if(pos.match(/^\-RRB\-$/)){
+			    $dom.append(")")
+			}else{
+			    $dom.append(word);
+			}
+			$dom.append(" ");
+		    });
+		});
+
+		var col_before = $dom.find("span.blank").css("color");
+		var col_after = $dom.css("background-color");
+		$dom.find("span.blank").css({"border-bottom": "1px solid",
+					     "border-bottom-color": col_before,
+					     "color": col_after});
+		
+		$dom.find("span.blank").click(function(){
+		    if( $(this).css("color") == col_before ){
+			$(this).css("color", col_after);		
+		    }else{
+			$(this).css("color", col_before);
+		    }
+		});
+
+		var b = true;
+		$dom.click(function(e){
+		    e.stopPropagation();
+		    if(e.altKey){
+			if(b){
+			    $dom.find("span.blank").css("color", col_before);
+			}else{
+			    $dom.find("span.blank").css("color", col_after);
+			}
+			b = !b;
+		    }
+		});
+	    });
 	}
     })
 })
 
-$(window).keydown(function (e) {
-    // the list of key codes
-    // http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+// $(window).keydown(function (e) {
+//     // the list of key codes
+//     // http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
 
-    if(e.altKey && e.which == 66){
-	listParentDoms();
-	return;
-    }    
-    if(e.altKey && e.shiftKey && e.which == 66){
-    	return;
-    }
-});
+//     if(e.altKey && e.which == 66){
+// 	listParentDoms();
+// 	return;
+//     }    
+//     if(e.altKey && e.shiftKey && e.which == 66){
+//     	return;
+//     }
+// });
--- railsBlanker/app/controllers/cont0_controller.rb
+++ railsBlanker/app/controllers/cont0_controller.rb
@@ -1,9 +1,52 @@
+require 'nokogiri'
+
 class Cont0Controller < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def blank
-    logger.debug "------ debug: cont0#blank -----"
-    logger.debug params[:sentence]
-    render :text => "coming from rails cont0#blank"
+    # logger.debug "------ debug: cont0#blank -----"
+    # logger.debug params[:sentence]
+    
+    input = Tempfile.new('input')
+    input << params[:sentence]
+    input.close
+
+    tagger_path = Pathname.new(Rails.root).parent + "stanford-postagger-2011-04-20"
+    # logger.debug "------ running tagger -----"
+    output = `java -cp #{tagger_path + "stanford-postagger.jar"} edu.stanford.nlp.tagger.maxent.MaxentTagger -model #{tagger_path + "models/left3words-wsj-0-18.tagger"} -textFile #{input.path}`
+    input.unlink
+
+    logger.debug "------ hopefully tagged sentence -----"
+    logger.debug output
+
+    render :text => split_tag(output)
+  end
+
+  
+  private
+
+  def split_tag(output)
+
+    # the list of POS labels
+    # http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.root {
+        output.split("\n").each do |s|
+          xml.s {
+            s.split(" ").each do |wt|
+              m = wt.match(/^(.*)\_([^\_]*)$/)
+              xml.wt {
+                if m && m.length >= 3
+                  xml.word m[1]
+                  xml.pos  m[2]
+                end
+              }
+            end
+          }
+        end
+      }
+    end
+
+    return builder.to_xml
   end
 end
